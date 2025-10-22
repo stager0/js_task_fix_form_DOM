318a57261d43160d77223c249b63e0fd51e3d203
Andrii <andriishtaher@gmail.com>
2025-10-22 02:30:15 +0200

    fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,21 @@
 'use strict';
 
 // write code here
-const inputsArr = document.querySelectorAll('input');
+const inputsArr = document.querySelectorAll('form input');
 
 for (const inputElement of inputsArr) {
   const tagToAdd = document.createElement('label');
   tagToAdd.classList.add('field-label');
-  tagToAdd.setAttribute('for', inputElement.id);
-  tagToAdd.textContent = inputElement.name;
+
+  tagToAdd.setAttribute('for', (inputElement.id || 'input-without-for'));
+  tagToAdd.textContent = inputElement.name || 'no-name';
 
   inputElement.parentElement.appendChild(tagToAdd);
-  inputElement.placeholder = inputElement.name.charAt(0).toUpperCase() + inputElement.name.slice(1);
+
+  if (inputElement.name) {
+    inputElement.placeholder = (inputElement.name.charAt(0).toUpperCase() + inputElement.name.slice(1));
+  } else {
+    inputElement.placeholder = "no-name";
+  }
+
 }
