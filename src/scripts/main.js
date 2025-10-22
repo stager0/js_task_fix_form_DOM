b7eff58841b1d494f5b8fe72bde31be14ac45a97
Andrii <andriishtaher@gmail.com>
2025-10-22 02:15:01 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write code here
+const inputsArr = document.querySelectorAll('input');
+
+for (const inputElement of inputsArr) {
+  const tagToAdd = document.createElement('label');
+  tagToAdd.classList.add('field-label');
+  tagToAdd.setAttribute('for', inputElement.id);
+  tagToAdd.textContent = inputElement.name;
+
+  inputElement.parentElement.appendChild(tagToAdd);
+  inputElement.placeholder = inputElement.name.charAt(0).toUpperCase() + inputElement.name.slice(1);
+}
